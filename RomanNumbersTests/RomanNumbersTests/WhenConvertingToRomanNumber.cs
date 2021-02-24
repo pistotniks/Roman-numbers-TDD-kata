22c6f1136901f161f994c36ee7b945c7a1b445bf
SCDOM\SEPI <sepi@simcorp.com>
2021-02-24 03:03:57 +0100

    Add a failed basic test to convert arabic 1 to roman number I

---
+++ RomanNumbersTests/RomanNumbersTests/WhenConvertingToRomanNumber.cs
@@ -0,0 +1,29 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace RomanNumbersTests
+{
+  public class WhenConvertingToRomanNumber
+  {
+    [Fact]
+    public void ShouldConvertArabicNumber()
+    {
+      var converter = new ToRomanNumberConverter();
+      const int arabicNumber = 1;
+      const string expectedRomanNumber = "I";
+
+      var romanNumber = converter.Convert(arabicNumber);
+
+      romanNumber.Should().Be(expectedRomanNumber);
+    }
+  }
+
+  public class ToRomanNumberConverter
+  {
+    public string Convert(int arabicNumber)
+    {
+      return null;
+    }
+  }
+}
