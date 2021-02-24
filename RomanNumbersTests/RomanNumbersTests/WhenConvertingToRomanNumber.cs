2f2a022ca313eaafa15c9d9339e55ede50d30c6b
SCDOM\SEPI <sepi@simcorp.com>
2021-02-24 03:29:00 +0100

    Pass the test 3 to III by adding an another conditional like a simulation of another method AddAdditionalI

--- RomanNumbersTests/RomanNumbersTests/WhenConvertingToRomanNumber.cs
+++ RomanNumbersTests/RomanNumbersTests/WhenConvertingToRomanNumber.cs
@@ -9,6 +9,7 @@ namespace RomanNumbersTests
     [Theory()]
     [InlineData(1, "I")]
     [InlineData(2, "II")]
+    [InlineData(3, "III")]
     public void ShouldConvertArabicNumber(int arabicNumber, string expectedRomanNumber)
     {
       var converter = new ToRomanNumberConverter();
@@ -26,6 +27,11 @@ namespace RomanNumbersTests
       string romanNumber = "I";
 
       if (arabicNumber > 1)
+      {
+        romanNumber += "I";
+      }
+
+      if (arabicNumber > 2)
       {
         romanNumber += "I";
       }
