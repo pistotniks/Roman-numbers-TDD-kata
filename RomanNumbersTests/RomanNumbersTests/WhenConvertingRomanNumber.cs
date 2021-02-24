b715115dfa30cb8fd6993750ed12a54f62e943b4
SCDOM\SEPI <sepi@simcorp.com>
2021-02-24 02:45:34 +0100

    Add a project and a test doing nothing

---
+++ RomanNumbersTests/RomanNumbersTests/WhenConvertingRomanNumber.cs
@@ -0,0 +1,15 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace RomanNumbersTests
+{
+  public class WhenConvertingRomanNumber
+  {
+    [Fact]
+    public void Nothing()
+    {
+      true.Should().BeTrue();
+    }
+  }
+}
